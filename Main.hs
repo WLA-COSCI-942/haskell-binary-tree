8126c312f011023678c70a79318bbf4164629b43
rashadg1030 <rashad.sasaki@gmail.com>
2018-11-03 00:34:18 -0700

    Added some fun lists functions.

--- Main.hs
+++ Main.hs
@@ -1,24 +1,41 @@
-data Tree a = Leaf 
-            | Node (Tree a) a (Tree a)
+data Tree a = Leaf | Node (Tree a) a (Tree a)
   deriving (Show, Eq)
+  
+playTree :: Tree Int
+playTree = Leaf
+
+listToTree :: (Eq a, Ord a) => [a] -> Tree a
+listToTree []  = Leaf
+listToTree [x] = insert x Leaf 
+listToTree (x:xs) = insert x (listToTree xs)
 
 insert :: (Eq a, Ord a) => a -> Tree a -> Tree a
 insert x Leaf = Node Leaf x Leaf
 insert x (Node rTree d lTree)
   | x < d = Node (insert x rTree) d lTree
   | x >= d = Node rTree d (insert x lTree)
+
+treeToList :: (Eq a, Ord a) => Tree a -> [a]
+treeToList Leaf = []
+treeToList (Node lTree value rTree) = (treeToList lTree) ++ [value] ++ (treeToList rTree)
   
-playTree :: Tree Int
-playTree = Leaf
-
-build :: (Eq a, Ord a) => [a] -> Tree a
-build [x] = insert x Leaf 
-build (x:xs) = insert x (build xs)
-
-inOrder :: (Eq a, Ord a) => Tree a -> [a]
-inOrder Leaf = []
-inOrder (Node lTree d rTree) = (inOrder lTree) ++ [d] ++ (inOrder rTree)
-  
-
 testTree :: Tree Int
-testTree = (build [1,5,3,4,8,0])
+testTree = listToTree [1,5,3,4,8,0]
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' [x] = [x]
+reverse' [x,y] = [y,x]
+reverse' (x:xs) = reverse xs ++ [x]
+
+magic :: (a -> b -> c) -> [a] -> [b] -> [c]
+magic _ [] [] = []
+magic f [] _  = []
+magic f _  [] = []
+magic f (a:as) (b:bs) = (f a b) : magic f as bs
+
+addLists :: [Int] -> [Int] -> [Int]
+addLists xs ys = magic (+) xs ys  
+
+mulLists :: [Int] -> [Int] -> [Int]
+mulLists xs ys = magic (*) xs ys
